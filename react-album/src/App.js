a3a6aa4a22d087bccf23347f537cafe80901a9a3
PunchCode <monetcarriere@yahoo.com>
2019-08-05 08:51:24 -0700

    final

---
+++ react-album/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  
+  
+  
+  
+  
+  render() {
+      return (
+        <Header
+        <h1>Hello World</h1>
+      )
+  }
+}
+
+export default App
